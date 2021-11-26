f392d4ad8fd1ec50d894bd5d80ab9be85bb76a7a
Santiago Moreno <santiago.moreno@nivelics.co>
2021-11-26 14:00:26 -0500

    Creacion del webpack

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome</h1>
+                <h1>Welcome Rama Santiago</h1>
                 <input placeholder="Username"></input>
                 <input placeholder="Password"></input>
                 <a href="/">Forgot Password</a>
