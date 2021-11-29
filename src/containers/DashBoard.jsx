6ba795ff9b48625267d9ca424c8867ac74c84c0d
Santiago Moreno <santiago.moreno@nivelics.co>
2021-11-29 12:08:26 -0500

    configuracion del package-json y primeras rutas

---
+++ src/containers/DashBoard.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+const DashBoard=()=>{
+
+
+    return(<h1>Pagina del dashboard</h1>)
+
+}
+export default DashBoard
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import DashBoard from '../containers/DashBoard';
 import Login from '../containers/LogIn';
 
 
 const App =()=>{
+    return(
     <BrowserRouter>
         <Routes>
             <Route  path = "/LogIn" element = {<Login/>}></Route>
-            
+            <Route path = "/Dashboard" element ={<DashBoard/>}></Route>
         </Routes>
-    </BrowserRouter>    
+    </BrowserRouter>  )  
 }
 export default App;
