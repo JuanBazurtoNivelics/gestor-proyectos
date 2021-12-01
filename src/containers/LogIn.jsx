23d10314d0dac1db965ead119998a0db3578a409
Juan Camilo Bazurto <camilobzt@gmail.com>
2021-12-01 15:46:43 -0500

    firebase y login

--- src/containers/LogIn.jsx
+++ src/containers/LogIn.jsx
@@ -5,6 +5,7 @@ const Login = ()=>{
 
 
     return (
+        
         <React.Fragment>
             <Home/  >
         </React.Fragment>
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAcsaGac0Ugq_ELseEySSG-ibAjptikqBU",
+  authDomain: "gestor-proyectos-nivelics.firebaseapp.com",
+  projectId: "gestor-proyectos-nivelics",
+  storageBucket: "gestor-proyectos-nivelics.appspot.com",
+  messagingSenderId: "167967778761",
+  appId: "1:167967778761:web:69d0674ccbceff3d63ea18",
+  measurementId: "G-E1CL1DYCRK",
+};
+
+const fire = firebase.initializeApp(firebaseConfig);
+const auth = fire.auth();
+
+export { auth };
