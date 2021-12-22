cb0d273dbe99b8ce51da87a3b79a7e31a09ecb3b
Juan Camilo Bazurto <camilobzt@gmail.com>
2021-12-22 15:27:42 -0500

    toolbarClick2.0

--- src/components/Gantt/index.jsx
+++ src/components/Gantt/index.jsx
@@ -7,21 +7,20 @@ import {
   Inject,
   Toolbar,
   Selection,
-  RowDD
+  Filter,
+  RowDD,
 } from "@syncfusion/ej2-react-gantt";
-import { ButtonComponent } from '@syncfusion/ej2-react-buttons'
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import { data } from "./datasource";
 import "./Gantt.css";
 
 const GanttDiagram = () => {
-
   let ganttInstance = GanttComponent;
 
   const editOptions = {
     allowEditing: true,
     allowAdding: true,
     allowDeleting: true,
-    showDeleteConfirmDialog: true,
     mode: "Auto",
     allowTaskbarEditing: true,
   };
@@ -35,44 +34,54 @@ const GanttDiagram = () => {
     child: "subtasks",
   };
 
-  const workWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  const workWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
   const labelValues = {
     // eslint-disable-next-line no-template-curly-in-string
-    taskLabel: '${Progress}%'
-  }
+    taskLabel: "${Progress}%",
+  };
 
-  const toolbarOptions = ['Edit', 'Delete', 'Cancel', 'Update', 'Search', 'Indent', 'Outdent'];
-  
-  const addProject= () => {
+  const toolbarOptions = [
+    "Edit",
+    "Delete",
+    "Cancel",
+    "Update",
+    "Search",
+    "Indent",
+    "Outdent",
+  ];
+
+  const addProject = () => {
     let dataProject = {
-        TaskName: 'New Added Project',
-        StartDate: new Date(),
-        subtasks: [
-          {}
-      ]
+      TaskName: "New Added Project",
+      StartDate: new Date(),
+      subtasks: [{}],
     };
     ganttInstance.editModule.addRecord(dataProject);
     ganttInstance.editModule.dialogModule.openEditDialog();
-}
-const addTask= () => {
-  
-  let dataTask = {
-    TaskName: 'New Added Task', StartDate: new Date()
-  };    
-  
-  ganttInstance.editModule.addRecord(dataTask, 'Child');
-  var id = Math.max(...ganttInstance.ids);
-  ganttInstance.editModule.dialogModule.openEditDialog(id);
-}
+  };
+  const addTask = () => {
+    let dataTask = {
+      TaskName: "New Added Task",
+      StartDate: new Date(),
+    };
 
-const toolbarClickHandler = () =>{
-  console.log('Hola');
-}
+    ganttInstance.editModule.addRecord(dataTask, "Child");
+    var id = Math.max(...ganttInstance.ids);
+    ganttInstance.editModule.dialogModule.openEditDialog(id);
+  };
+
+  const toolbarClickHandler = (args) => {
+    if (args.item.properties.tooltipText !== "Search") {
+      console.log("Changes");
+    }
+  };
 
   return (
     <div>
-      <ButtonComponent onClick={addProject.bind(this)}>Add Project</ButtonComponent>
+      <ButtonComponent onClick={addProject.bind(this)}>
+        Add Project
+      </ButtonComponent>
       <ButtonComponent onClick={addTask.bind(this)}>Add Task</ButtonComponent>
       <GanttComponent
         dataSource={data}
@@ -80,20 +89,20 @@ const toolbarClickHandler = () =>{
         toolbar={toolbarOptions}
         height="550px"
         timelineSettings={{ timelineViewMode: "Week" }}
-        gridLines='Both'
+        gridLines="Both"
         editSettings={editOptions}
-        taskMode='Auto'
+        taskMode="Auto"
         allowSelection={true}
         allowRowDragAndDrop={true}
         rowHeight={50}
         labelSettings={labelValues}
-        splitterSettings={{position:"40%"}}
+        splitterSettings={{ position: "40%" }}
         workWeek={workWeek}
         selectedRowIndex={0}
-        ref={gantt => ganttInstance = gantt}
+        ref={(gantt) => (ganttInstance = gantt)}
         toolbarClick={toolbarClickHandler}
       >
-        <Inject services={[RowDD, Edit, Toolbar, Selection]} />
+        <Inject services={[Filter, RowDD, Edit, Toolbar, Selection]} />
         <ColumnsDirective>
           <ColumnDirective field="TaskID" headerText="ID" textAlign="Center" />
           <ColumnDirective
@@ -110,6 +119,7 @@ const toolbarClickHandler = () =>{
         </ColumnsDirective>
       </GanttComponent>
     </div>
-  );}
+  );
+};
 
 export default GanttDiagram;
